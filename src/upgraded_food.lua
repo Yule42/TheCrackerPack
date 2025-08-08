19984a5b6a7dae11bcbd8f3dfcdaddbbfbf503d6
Yule42 <gamesandstuff4ever@gmail.com>
2025-08-08 15:25:15 -0700

    grievous credits error

--- src/upgraded_food.lua
+++ src/upgraded_food.lua
@@ -314,7 +314,7 @@ SMODS.Joker{ --Can of Beans
     
 
     loc_vars = function(self, info_queue, card)
-        if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'None', 'sugariimari'}, key = 'artist_credits_cracker'} end
+        if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimari'}, key = 'artist_credits_cracker'} end
         return {vars = {card.ability.extra.rounds, card.ability.extra.rounds_remove, card.ability.extra.hand_size}}
     end,
     
