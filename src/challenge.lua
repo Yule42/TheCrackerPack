7ba66b07ef0bc4db2b5ea6adac5bf03947a67830
Yule42 <gamesandstuff4ever@gmail.com>
2025-08-09 13:35:49 -0700

    vanilla challenges adjustment

--- src/challenge.lua
+++ src/challenge.lua
@@ -1,3 +1,55 @@
+function Cracker.get_challenge_by_id(challenges_table, id) -- G.CHALLENGES for vanilla challenges
+    for _, challenge in pairs(challenges_table) do
+        if challenge.id == id then
+            print(challenge)
+            return challenge
+        end
+    end
+    return nil
+end
+local omelette = Cracker.get_challenge_by_id(G.CHALLENGES, "c_omelette_1")
+local nonperishable = Cracker.get_challenge_by_id(G.CHALLENGES, "c_non_perishable_1")
+local blastoff = Cracker.get_challenge_by_id(G.CHALLENGES, "c_blast_off_1")
+local fivecard = Cracker.get_challenge_by_id(G.CHALLENGES, "c_five_card_1")
+local goldenneedle = Cracker.get_challenge_by_id(G.CHALLENGES, "c_golden_needle_1")
+
+if omelette then
+    table.insert(omelette.restrictions.banned_cards, 2, {id = 'v_cracker_silver_spoon'})
+    table.insert(omelette.restrictions.banned_cards, 2, {id = 'v_cracker_heirloom'})
+    table.insert(omelette.restrictions.banned_cards, {id = 'j_cracker_chocolatecoin'})
+    if not disable_card then
+        table.insert(omelette.restrictions.banned_cards, {id = 'j_cracker_yellowcard'})
+    end
+end
+
+if nonperishable then
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_chocolatecoin'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_saltinecracker'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_grahamcracker'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_cheese'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_crackerbarrel'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_curry'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_potatochips'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_shrimpcocktail'})
+    table.insert(nonperishable.restrictions.banned_cards, 8, {id = 'j_cracker_hamburger'})
+end
+
+if blastoff then
+    table.insert(blastoff.restrictions.banned_cards, {id = 'j_cracker_knifethrower'})
+    table.insert(blastoff.restrictions.banned_cards, {id = 'j_cracker_hamburger'})
+end
+
+if fivecard then
+    if not disable_card then
+        table.insert(fivecard.restrictions.banned_cards, {id = 'j_cracker_pinkcard'})
+    end
+end
+
+if goldenneedle then
+    table.insert(goldenneedle.restrictions.banned_cards, {id = 'j_cracker_knifethrower'})
+    table.insert(goldenneedle.restrictions.banned_cards, {id = 'j_cracker_hamburger'})
+end
+
 SMODS.Challenge{
     loc_txt = {name = "Hot Potato"},
     key = "hotpotato",
