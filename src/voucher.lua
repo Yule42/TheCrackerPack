51fbfd93bc58b006da4ec04643066e671643fce1
Yule42 <gamesandstuff4ever@gmail.com>
2025-08-12 00:14:33 -0700

    Fix crash

--- src/voucher.lua
+++ src/voucher.lua
@@ -68,7 +68,7 @@ SMODS.Voucher {
         }
     },
     loc_vars = function(self, info_queue, card)
-        if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimari'}, key = 'artist_credits_cracker'} end
+        if card and card.area and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimari'}, key = 'artist_credits_cracker'} end
         return {vars = {card.ability.extra.weight_mod}}
     end,
     redeem = function(self)
@@ -116,7 +116,7 @@ if not CrackerConfig.disable_tier3 then
         },
         requires = { "v_nacho_tong" },
         loc_vars = function(self, info_queue, card)
-            if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimari'}, key = 'artist_credits_cracker'} end
+            if card and card.area and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimari'}, key = 'artist_credits_cracker'} end
             return {vars = {card.ability.extra.hands}}
         end,
         
