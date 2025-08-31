be001a083a1eb3c55316af3f27e0dcaca546ffd8
Sophie <38859940+Yule42@users.noreply.github.com>
2025-08-30 20:15:54 -0700

    Mail Call

--- src/challenge.lua
+++ src/challenge.lua
@@ -148,6 +148,76 @@ if not disable_card then
 end
 
 SMODS.Challenge{
+    loc_txt = {name = "Mail Call"},
+    key = "mailcall",
+    jokers = {
+        {id = 'j_cracker_postman', eternal = true},
+    },
+    vouchers = {
+    },
+    rules = {
+        custom = {
+        }
+    },
+    deck = {
+        cards = {
+			{ s = "D", r = "2", g = "Gold" },
+			{ s = "D", r = "3" },
+			{ s = "D", r = "4" },
+			{ s = "D", r = "5" },
+			{ s = "D", r = "6" },
+			{ s = "D", r = "7" },
+			{ s = "D", r = "8" },
+			{ s = "D", r = "9" },
+			{ s = "D", r = "T" },
+			{ s = "D", r = "J" },
+			{ s = "D", r = "Q" },
+			{ s = "D", r = "K" },
+			{ s = "D", r = "A" },
+			{ s = "C", r = "2", g = "Blue" },
+			{ s = "C", r = "3" },
+			{ s = "C", r = "4" },
+			{ s = "C", r = "5" },
+			{ s = "C", r = "6" },
+			{ s = "C", r = "7" },
+			{ s = "C", r = "8" },
+			{ s = "C", r = "9" },
+			{ s = "C", r = "T" },
+			{ s = "C", r = "J" },
+			{ s = "C", r = "Q" },
+			{ s = "C", r = "K" },
+			{ s = "C", r = "A" },
+			{ s = "H", r = "2", g = "Red" },
+			{ s = "H", r = "3" },
+			{ s = "H", r = "4" },
+			{ s = "H", r = "5" },
+			{ s = "H", r = "6" },
+			{ s = "H", r = "7" },
+			{ s = "H", r = "8" },
+			{ s = "H", r = "9" },
+			{ s = "H", r = "T" },
+			{ s = "H", r = "J" },
+			{ s = "H", r = "Q" },
+			{ s = "H", r = "K" },
+			{ s = "H", r = "A" },
+			{ s = "S", r = "2", g = "Purple" },
+			{ s = "S", r = "3" },
+			{ s = "S", r = "4" },
+			{ s = "S", r = "5" },
+			{ s = "S", r = "6" },
+			{ s = "S", r = "7" },
+			{ s = "S", r = "8" },
+			{ s = "S", r = "9" },
+			{ s = "S", r = "T" },
+			{ s = "S", r = "J" },
+			{ s = "S", r = "Q" },
+			{ s = "S", r = "K" },
+			{ s = "S", r = "A" },
+		},
+    }
+}
+
+SMODS.Challenge{
     loc_txt = {name = "The End"},
     key = "theend2",
     jokers = {
