a181b8e4488597b2c3de136970ad882e0291c695
Sophie <38859940+Yule42@users.noreply.github.com>
2025-09-26 10:45:53 -0700

    woah haha

--- src/expansion_2/jokers.lua
+++ src/expansion_2/jokers.lua
@@ -6,8 +6,8 @@ SMODS.Joker{ --Royal Decree
         }
     },
     pos = {
-        x = 8,
-        y = 2,
+        x = 9,
+        y = 4,
     },
     cost = 6,
     rarity = 2,
@@ -20,7 +20,7 @@ SMODS.Joker{ --Royal Decree
     
 
     loc_vars = function(self, info_queue, card)
-        if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'None', 'sugariimari'}, key = 'artist_credits_cracker'} end
+        if card and card.area.config.collection then info_queue[#info_queue+1] = {set = 'Other', vars = {'mrkyspices', 'sugariimarii'}, key = 'artist_credits_cracker'} end
         return {vars = {}}
     end,
 }
