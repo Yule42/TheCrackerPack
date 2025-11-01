e54cce6646c56ae4a777e5f19213aa950e66f3f4
Sophie <38859940+Yule42@users.noreply.github.com>
2025-11-01 15:11:36 -0700

    Orange card is now common

--- src/expansion_1/card_jokers.lua
+++ src/expansion_1/card_jokers.lua
@@ -258,7 +258,7 @@ SMODS.Joker{ --Orange Card
         y = 2
     },
     cost = 6,
-    rarity = 2,
+    rarity = 1,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
