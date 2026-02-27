b116e531d7041a13d26bc34a1e65ca3e0cf98789
Sophie <38859940+Yule42@users.noreply.github.com>
2026-02-26 21:45:45 -0800

    Actually actually this time

--- src/compat/JokerDisplay.lua
+++ src/compat/JokerDisplay.lua
@@ -11,7 +11,7 @@ JokerDisplay.Definitions.j_cracker_saltinecracker = {
         }
     },
     text_config = { colour = G.C.CHIPS },
-    text_config = { colour = G.C.GREEN, scale = 0.3 },
+    extra_config = { colour = G.C.GREEN, scale = 0.3 },
     calc_function = function(card)
         local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'Saltine Cracker')
         card.joker_display_values.odds = localize { type = 'variable', key = "jdis_odds", vars = { numerator, denominator } }
