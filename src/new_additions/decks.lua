1441066f841a0999cf142e98b8313ed4db681c54
Sophie <38859940+Yule42@users.noreply.github.com>
2026-07-11 11:01:57 -0700

    solar deck interal

--- src/new_additions/decks.lua
+++ src/new_additions/decks.lua
@@ -66,50 +66,53 @@ SMODS.Back{ -- Solar Deck
     end,
     
     apply = function(self, back)
-        G.GAME.hands['High Card'].l_chips = 15
-        G.GAME.hands['Pair'].l_chips = 20
-        G.GAME.hands['Two Pair'].l_chips = 35
-        G.GAME.hands['Two Pair'].l_mult = 2
-        G.GAME.hands['Three of a Kind'].l_chips = 35
-        G.GAME.hands['Three of a Kind'].l_mult = 3
-        G.GAME.hands['Straight'].l_chips = 50
-        G.GAME.hands['Straight'].l_mult = 5
-        G.GAME.hands['Flush'].l_chips = 25
-        G.GAME.hands['Flush'].l_mult = 4
-        G.GAME.hands['Full House'].l_chips = 50
-        G.GAME.hands['Full House'].l_mult = 5
-        G.GAME.hands['Four of a Kind'].l_chips = 50
-        G.GAME.hands['Four of a Kind'].l_mult = 6
-        G.GAME.hands['Straight Flush'].l_chips = 70
-        G.GAME.hands['Straight Flush'].l_mult = 8
-        G.GAME.hands['Five of a Kind'].l_chips = 65
-        G.GAME.hands['Five of a Kind'].l_mult = 6
-        G.GAME.hands['Flush House'].l_chips = 80
-        G.GAME.hands['Flush House'].l_mult = 8
-        G.GAME.hands['Flush Five'].l_chips = 80
-        G.GAME.hands['Flush Five'].l_mult = 6
+        local hand_values = {
+            ["High Card"]      = {chips = 15},
+            ["Pair"]           = {chips = 20},
+            ["Two Pair"]       = {chips = 35, mult = 2},
+            ["Three of a Kind"]= {chips = 35, mult = 3},
+            ["Straight"]       = {chips = 50, mult = 5},
+            ["Flush"]          = {chips = 25, mult = 4},
+            ["Full House"]     = {chips = 50, mult = 5},
+            ["Four of a Kind"] = {chips = 50, mult = 6},
+            ["Straight Flush"] = {chips = 70, mult = 8},
+            ["Five of a Kind"] = {chips = 65, mult = 6},
+            ["Flush House"]    = {chips = 80, mult = 8},
+            ["Flush Five"]     = {chips = 80, mult = 6},
+        }
+        local spec_name
         if next(SMODS.find_mod('Bunco')) and G.GAME.hands['bunc_Spectrum'] then
-            G.GAME.hands['bunc_Spectrum'].l_chips = 30
-            G.GAME.hands['bunc_Spectrum'].l_mult = 3
-            G.GAME.hands['bunc_Straight Spectrum'].l_chips = 80
-            G.GAME.hands['bunc_Straight Spectrum'].l_mult = 8
-            G.GAME.hands['bunc_Spectrum House'].l_chips = 75
-            G.GAME.hands['bunc_Spectrum House'].l_mult = 8
-            G.GAME.hands['bunc_Spectrum Five'].l_chips = 100
-            G.GAME.hands['bunc_Spectrum Five'].l_mult = 6
+            spec_name = "bunc"
         elseif next(SMODS.find_mod('paperback')) and G.GAME.hands['paperback_Spectrum'] then
-            G.GAME.hands['paperback_Spectrum'].l_chips = 30
-            G.GAME.hands['paperback_Spectrum'].l_mult = 3
-            G.GAME.hands['paperback_Straight Spectrum'].l_chips = 80
-            G.GAME.hands['paperback_Straight Spectrum'].l_mult = 8
-            G.GAME.hands['paperback_Spectrum House'].l_chips = 75
-            G.GAME.hands['paperback_Spectrum House'].l_mult = 8
-            G.GAME.hands['paperback_Spectrum Five'].l_chips = 100
-            G.GAME.hands['paperback_Spectrum Five'].l_mult = 6
+            spec_name = "paperback"
+        end
+        if spec_name then
+            hand_values[spec_name .. "_Spectrum"] = {chips = 30, mult = 3}
+            hand_values[spec_name .. "_Straight Spectrum"] = {chips = 80, mult = 8}
+            hand_values[spec_name .. "_Spectrum House"] = {chips = 75, mult = 8}
+            hand_values[spec_name .. "_Spectrum Five"] = {chips = 100, mult = 6}
         end
         if next(SMODS.find_mod('allinjest')) and G.GAME.hands['aij_Royal Flush'] then
-            G.GAME.hands['aij_Royal Flush'].l_chips = 100
-            G.GAME.hands['aij_Royal Flush'].l_mult = 12
+            hand_values["aij_Royal Flush"] = {chips = 100, mult = 12}
+        end
+        if next(SMODS.find_mod('artbox')) and G.GAME.hands['artb_null'] then
+            hand_values["artb_null"] = {chips = 45, mult = 6}
+        end
+        --[[if next(SMODS.find_mod('Maximus')) and G.GAME.hands['mxms_f_three_pair'] then -- i'll finish this later
+            hand_values["mxms_three_pair"] = {chips = 40, mult = 1}
+            hand_values["mxms_double_triple"] = {chips = 70, mult = 5}
+            hand_values["mxms_6oak"] = {chips = 70, mult = 7}
+            hand_values["mxms_s_straight"] = {chips = 80, mult = 5}
+            hand_values["mxms_s_flush"] = {chips = 35, mult = 5}
+            hand_values["mxms_house_party"] = {chips = 75, mult = 6}
+            hand_values["mxms_f_three_pair"] = {chips = 35, mult = 5}
+            hand_values["mxms_f_double_triple"] = {chips = 100, mult = 9}
+        end]]
+        for hand, values in pairs(hand_values) do
+            if G.GAME.hands[hand] then
+                if values.chips then G.GAME.hands[hand].l_chips = values.chips end
+                if values.mult then G.GAME.hands[hand].l_mult = values.mult end
+            end
         end
     end,
 }
