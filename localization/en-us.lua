ef4e8b95f8583644c75f9857a10e496fed0b3f80
Sophie <38859940+Yule42@users.noreply.github.com>
2026-09-15 13:07:29 -0700

    update loc

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1125,6 +1125,12 @@ return {
                     "{C:inactive}(Gives upgrade if voucher already owned)"
                 }
             },
+            v_cracker_pw_solar = {
+                name = 'Solar Deck',
+                text = {
+                    '{C:planet}Planet{} cards are stronger',
+                }
+            },
             v_cracker_pw_white = {
                 name = 'White Deck',
                 text = {
